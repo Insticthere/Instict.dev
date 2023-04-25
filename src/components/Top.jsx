10be06e18bb7005ad79db3f0506b05591d3dd350
Insticthere <omgnitingupta@gmail.com>
2023-04-26 04:10:41 +0530

    change before spotify

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import 'dayjs/locale/en';
+
+dayjs.extend(utc); // Load utc plugin
+dayjs.extend(timezone); // Load timezone plugin
+dayjs.tz.setDefault('America/New_York'); 
 
 function Top() {
-
+  const [localTime, setLocalTime] = useState(dayjs()); // Use state to store the local time
   const [isHovered, setIsHovered] = useState(false);
-  
+
   const handleHover = (event) => {
     document.getElementsByClassName("App")[0].classList.remove("unhovered");
     document.getElementsByClassName("App")[0].classList.add("hovered");
@@ -17,9 +25,12 @@ function Top() {
   };
 
   useEffect(() => {
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove)});
-  
+    const intervalId = setInterval(() => {
+      setLocalTime(dayjs()); // Update the local time every second
+    }, 1000);
+    return () => clearInterval(intervalId); // Clear the interval on unmount
+  }, []);
+
   function handleMouseMove(event) {
     const elem = document.querySelector(".parallax");
     let _w = window.innerWidth / 2;
@@ -34,18 +45,19 @@ function Top() {
     elem.style.backgroundPosition = x;
   }
     
-    return (
-        <div onMouseEnter={handleHover} onMouseLeave={handleMouseLeave} onMouseMove={handleMouseMove} className="flex justify-center content-center flex-row gap-[1.25rem] mt-[10%] mx-auto mt-10px ">
-          <div className="flex justify-items-center items-center">
-            <p className="mr-[10px] text-[6rem] m-0  font-sans sm:text-[7rem] lg:text-[8.4rem] ">Instict</p>
-          </div>
-          <div>
-            <img src="https://i.ibb.co/xmMtYjv/avatar2.jpg" alt="pfp" className="rounded-[11px] max-w-[275px] w-[100%] h-auto hover:scale-[1.05] hover:origin-top-left ease-in-out duration-300"></img>
-          </div>
-
-
+  return (
+    <div onMouseEnter={handleHover} onMouseLeave={handleMouseLeave} onMouseMove={handleMouseMove} className="flex justify-center content-center flex-row gap-[1.25rem] mt-[10%] mx-auto mt-10px ">
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-items-center items-center">
+          <p className="mr-[10px] text-[6rem] m-0  font-sans sm:text-[7rem] lg:text-[8.4rem] ">Instict</p>
         </div>
-    );
-  }
-  
-  export default Top;
+        <p>{localTime.format('YYYY-MM-DD')} · {localTime.format("hh:mm:ss A")} </p>
+      </div>
+      <div>
+        <img src="https://i.ibb.co/xmMtYjv/avatar2.jpg" alt="pfp" className="rounded-[11px] max-w-[275px] w-[100%] h-auto hover:scale-[1.05] hover:origin-top-left ease-in-out duration-300"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Top;
