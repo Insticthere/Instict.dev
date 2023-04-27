268ffdc6008f69aacc4e62dab7dd6cafcbb26ee9
Insticthere <omgnitingupta@gmail.com>
2023-04-27 21:26:17 +0530

    socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,27 +1,85 @@
 import React from "react";
 
-function Projects() {
+function Socials() {
     
     return (
-        <div className="projects">
-          <div className="projectheading">
-            <h2>Contact Me</h2>
+          <div className="flex gap-3  justify-end pt-5 pr-4 ">
+            <div className="group">
+              <a href="https://github.com/Insticthere" target="_blank" rel="noopener noreferrer">
+                <div className="flex items-center">
+                      <div
+                      className="fa fa-github hover:scale-[1.1]"
+                      style={{
+                        fontSize: "30px",
+                        color: "white",
+                        backgroundColor: "transparent",
+                        borderRadius: "100%"
+                      }}></div>
+                      
+                </div>
+              </a>
+            </div>
+            <div className="group" target="_blank" rel="noopener noreferrer">
+                      <a href="https://open.spotify.com/user/xp36gr2k8ragq465cl5mg2sa9">
+              <div className="flex items-center">
+                    <div
+                    className="fa fa-spotify hover:scale-[1.1]"
+                    style={{
+                      fontSize: "30px",
+                      color: "#1DB954",
+                      backgroundColor: "transparent",
+                      borderRadius: "100%"
+                    }}></div>
+                    
+              </div>
+              </a>
+            </div>
+            <div className="group" target="_blank" rel="noopener noreferrer">
+            <a href="https://discordapp.com/users/522317353917087745/">
+              <div className="flex items-center">
+                    <div
+                    className="fa-brands fa-discord hover:scale-[1.1]"
+                    style={{
+                      fontSize: "30px",
+                      color: "#5865F2",
+                      backgroundColor: "transparent",
+                      borderRadius: "100%"
+                    }}></div>
+                    
+              </div>
+            </a>
+            </div>
+            <div className="group">
+            <a href="mailto:queriesinstict@gmail.com">
+              <div className="flex items-center">
+                    <div
+                    className="fa fa-envelope hover:scale-[1.1]"
+                    style={{
+                      fontSize: "30px",
+                      color: "white",
+                      backgroundColor: "transparent",
+                      borderRadius: "100%"
+                    }}></div>
+                    
+              </div>
+            </a>
+            </div>
           </div>
-          <div>
-            <ul className="socials">
-              <li
-                className="fa fa-reddit"
-                style={{
-                  fontSize: "48px",
-                  color: "red",
-                  backgroundColor: "black",
-                  borderRadius: "100%"
-                }}></li>
-              <li>ok</li>
-            </ul>
-          </div>
-        </div>
     );
   }
   
-  export default Projects;
+  export default Socials;
+
+              {/* <div className="group w-fit">
+              <div className="flex items-center  group-hover:bg-custom rounded-full h-12  w-fit test">
+                  <div
+                    className="fa fa-reddit group-hover:scale-110"
+                    style={{
+                      fontSize: "48px",
+                      color: "#fd4500",
+                      backgroundColor: "transparent",
+                      borderRadius: "100%"
+                    }}></div>
+                    <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-black px-2">okokok</p>
+              </div>
+            </div> */}
--- src/pages/App.js
+++ src/pages/App.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Top from "../components/Top";
 import Activity from "../components/Activity"
 import Toptracks from "../components/TopTracks"
+import Socials from "../components/Socials"
 import React, { useRef, useEffect } from 'react';
 
 function App() {
@@ -23,11 +24,13 @@ function App() {
       <div ref={contentsRef} className="contents1 max-w-[1000px] w-[95%] grid grid-row-2">
         <div ref={targetRef}>
           <Top />
+          <Socials />
         </div>
-        <div className="grid grid-row-2 sm:grid-cols-2 mt-[5%] gap-[10px]">
+        <div className="grid grid-row-2 sm:grid-cols-2 mt-[7%] gap-[10px]">
           <Activity />
           <Toptracks />
         </div>
+        
       </div>
     </div>
   );
