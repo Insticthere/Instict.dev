8d225b83fb2df3d7ef1fcae59fdab3eafdaef99e
Insticthere <omgnitingupta@gmail.com>
2023-04-27 21:26:17 +0530

    socials

--- src/components/TopTracks.jsx
+++ src/components/TopTracks.jsx
@@ -20,17 +20,19 @@ function Toptracks() {
     return (
         Toptracks ? 
 
-        <div className=" overflow-x-auto" >
-            <div className="flex items-baseline">
+        <div >
+            <div className="flex items-baseline w-fit">
                 <h1 className="text-white mt-[10px] mb-[10px] text-xl font-extrabold">Top Tracks</h1>
-                <p> - last month</p>
+                <p className="pl-[5px]"> - last month</p>
             </div>
-            <div className="flex">
-              {Toptracks.items.map((element, key) => (
-                  <div key={key}>
-                      <img src={element.album.images[0].url } alt="ok" className="w-[100px] max-w-none"></img>
-                      {element.name}
-                  </div>))}
+            <div className="overflow-x-auto">
+              <div className="flex">
+                {Toptracks.items.map((element, key) => (
+                    <div key={key} className="pl-[5px]">
+                        <img src={element.album.images[0].url } alt="ok" className="w-[100px] max-w-none rounded-md"></img>
+                        <p className="text-[15px]">{element.name}</p>
+                    </div>))}
+              </div>
             </div>
             </div> : <></>
     );
