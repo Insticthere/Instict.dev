c41ecc97af0ab69feaa00592d8d1df560c2914ef
Insticthere <omgnitingupta@gmail.com>
2023-05-13 01:23:41 +0530

    THE CHANGE AFTER THI GON BE BIG

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,8 +19,8 @@ function Navbar() {
   <div className="sticky z-20 backdrop-blur-md sm:rounded-md w-full max-w-[calc(var(--pgwidth)+10px)] m-auto drop-shadow top-0 sm:top-[15px]">
     <header className="flex justify-between text-[20px] top-0 z-10 p-2 relative items-center sm:rounded-md">
       <div className="flex gap-3 pl-1">
-        <a className="text-sm sm:text-base" href="/">/Home</a>
-        <a className="text-sm sm:text-base" href="/songs">/Songs</a>
+        <a className="text-sm sm:text-base" href="/">Home</a>
+        <a className="text-sm sm:text-base" href="/songs">Songs</a>
       </div>
       <div>
         <button onClick={handleDarkModeToggle} className='w-7 h-7 max-sm:h-8 flex align-middle items-center justify-center'>{dark ? <img src="/icons/sun.png" className=" font-extrabold h-5 w-5"></img> : <img src="/icons/moon.png" className="fill-white font-extrabold invert h-5 w-5"></img>}</button>
