70dee8b8ea53bf807eb7db279db6cc81b2b6a844
Insticthere <omgnitingupta@gmail.com>
2023-05-29 20:08:38 +0530

    vercel routes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -13,7 +13,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/songs" element={<Songs />} />
-            <Route path="/discord" element="lol.com"/>
           </Routes>
         </div>
         
