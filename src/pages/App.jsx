f1cf2bd00aeea55b82eec3453dc4e0c6007fe669
Insticthere <omgnitingupta@gmail.com>
2023-06-06 19:35:36 +0530

    fixed footer 'f'

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Footer from "../components/footer";  
+import Footer from "../components/Footer";
 import Home from "./Home";
 import Songs from "./Songs";
 
